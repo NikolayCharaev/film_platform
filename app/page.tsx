7da149afa801f500246051bb63c6229395f61bf5
Good_Cthulhu <hayyabusa_01@mail.ru>
2024-01-16 20:29:07 +0300

    project init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+   <>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem eaque eius reprehenderit earum debitis. Illum dolores soluta consectetur dolorem. Porro dolorem quia consectetur tenetur unde laudantium veritatis, non sapiente nihil.
+   </>
+  )
+}
